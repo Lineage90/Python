dbe55dda29153533b0dfef7509756ee3a4ed58e4
Laneige90 <kjw1243@naver.com>
2023-10-16 18:09:23 +0900

    파이썬 알고리즘

---
+++ Algorithm/day1.py
@@ -0,0 +1,56 @@
+# 1. 그룹 아나그램
+# import collections
+
+# strs = ["eat","tea","tan","ate","nat","bat"]
+
+
+# 책 풀이
+# import collections
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+#         # 풀이
+#         anagrams = collections.defaultdict(list)
+
+#         for word in strs:
+#             # 정렬하여 딕셔너리에 추가
+#             anagrams[''.join(sorted(word))].append(word)
+
+#         return anagrams.values()    
+
+# 2. 가장 긴 팰린드롬
+
+# 3. 세 수의 합
+
+
+
+# 4. 배열 파티션
+
+# nums = [6,2,6,5,1,2]
+# nums.sort()
+# result = []
+# add = 0
+
+# for i in range(0, len(nums), 2):
+#     result.append(nums[i:i+2])
+
+# for j in range(len(result)):
+#     add += min(result[j])
+
+
+# 책 풀이
+
+# nums = [1,4,3,2]
+
+# sum = 0
+# pair = []
+# nums.sort()
+
+# for n in nums:
+#     # 앞에서부터 오름차순으로 페어를 만들어 합산
+#     pair.append(n)
+#     if len(pair) == 2:
+#         sum += min(pair)
+#         pair = []
+
+# return sum
