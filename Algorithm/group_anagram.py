242bab7d7a056d5f13546e438cf6007796666ac6
Laneige90 <kjw1243@naver.com>
2023-10-18 00:06:02 +0900

    알고리즘 문제 풀이

--- Algorithm/group_anagram.py
+++ Algorithm/group_anagram.py
@@ -1,25 +1,14 @@
 # 1. 그룹 아나그램
-# import collections
+import collections
 
-# strs = ["eat","tea","tan","ate","nat","bat"]
+# 풀이
+strs = ["eat","tea","tan","ate","nat","bat"]
+    # 풀이
+    
+anagrams = collections.defaultdict(list)
 
-# 
-# for str in strs:
-#   s = ''.join(sorted(str))
-#   result[s] = result.get(s,[]) + [str]
-# return result.values()
+for word in strs:
+            # 정렬하여 딕셔너리에 추가
+    anagrams[''.join(sorted(word))].append(word)
 
-
-# 책 풀이
-# import collections
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-#         # 풀이
-#         anagrams = collections.defaultdict(list)
-
-#         for word in strs:
-#             # 정렬하여 딕셔너리에 추가
-#             anagrams[''.join(sorted(word))].append(word)
-
-#         return anagrams.values()    
+print(anagrams.values())
