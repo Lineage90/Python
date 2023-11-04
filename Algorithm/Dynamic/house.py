c12559715bb763864ceb974837efb53511976deb
Laneige90 <kjw1243@naver.com>
2023-11-04 13:25:31 +0900

    파이썬 알고리즘 문제풀이

---
+++ Algorithm/Dynamic/house.py
@@ -0,0 +1,41 @@
+# 1
+# nums = [2,7,9,3,1]
+
+# result_a = 0
+# result_b = 0
+
+# for i in range(0, len(nums), 2):
+#     result_a += nums[i]
+
+# for j in range(1, len(nums), 2):
+#     result_b += nums[j]
+
+
+# if result_a > result_b:
+#     print(result_a)
+# else:
+#     print(result_b)
+
+# [2,1,1,2] 오류
+
+# 2
+nums = [2,7,9,3,1]
+
+dp = [0 for _ in range(len(nums))]
+
+# 배열에 값 없으면 0
+if not nums:
+    print(0)
+
+# 배열 길이가 2 이하이면 최고 큰 숫자가 답
+if len(nums) <= 2:
+    print(max(nums))
+
+# dp[0]에 nums[0], dp[1]에 nums[0] & num[1] 중 큰수 
+dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+
+
+for i in range(2, len(nums)):
+    dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+
+print(dp.pop())
