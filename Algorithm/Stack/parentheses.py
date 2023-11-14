cbabd8552562223c499078cfeaf32c406f8492a2
Laneige90 <kjw1243@naver.com>
2023-11-14 09:31:01 +0900

    파이썬 알고리즘 문제풀이

--- Algorithm/Stack/parentheses.py
+++ Algorithm/Stack/parentheses.py
@@ -3,7 +3,9 @@
 # 주어지는 N개의 괄호 문자열이 VPS 인지 판단해 YES NO 를 반환하라
 
 # VPS : () 로 이루어진 한쌍의 괄호
+import sys
 
+input = sys.stdin.readline
 
 N = int(input())
 
@@ -19,8 +21,10 @@ for _ in range(N):
                 break
             else:
                 a.pop()
+    # 리스트 길이가 0이 아니면 NO           
     if len(a) != 0:
         print('NO')
+    # 맞으면 YES
     else:
         print('YES')
                 
