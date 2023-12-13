ca691f451394d02bfbcd7b85421c1e9028a530e9
Laneige90 <kjw1243@naver.com>
2023-12-13 09:58:13 +0900

    파이썬 알고리즘 문제풀이

---
+++ Algorithm/Dynamic/파도반수열.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline()
+
+t = int(input())
+
+li = [1,1,1,2,2]
+for i in range(5, 100):
+    li.append(li[i-1]+li[i-5])
+
+for _ in range(t):
+    n = int(input())
+    print(li[n-1])
